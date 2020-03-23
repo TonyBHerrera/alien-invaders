de709a1f72bdd5999dd3b335786d582d9ca5a591
TonyBHerrera <tonybherrera@yahoo.com>
2020-03-23 08:54:32 -0600

    creating rows of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,20 +79,28 @@ class AlienInvasion:
         """Create the fleet of aliens."""
         # Make an alien.
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2* alien_width)
 
+        # Determine the number of rows of aliens that fit on the screen.
+        shit_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - 
+                                (3 * alien_height) - shit_height)
+        number_rows = available_space_y // (2 * alien_height)                        
+
         # Create the first row of aliens
-        for alien_number in range(number_aliens_x):
-            # Create an alien and place it in th row
-            self._create_alien(alien_number)
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
     
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
             alien = Alien(self)
+            alien_width, alien_height = alien.rect.size
             alien_width = alien.rect.width
             alien.x = alien_width + 2 * alien_width * alien_number
             alien.rect.x = alien.x
+            alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
             self.aliens.add(alien)        
     
     def _update_screen(self):
