7e20c116e9f7e36b1101e02db62b6ed9b699b57c
TonyBHerrera <tonybherrera@yahoo.com>
2020-03-22 20:27:57 -0600

    refactoring _create_fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,7 +79,21 @@ class AlienInvasion:
         """Create the fleet of aliens."""
         # Make an alien.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2* alien_width)
+
+        # Create the first row of aliens
+        for alien_number in range(number_aliens_x):
+            # Create an alien and place it in th row
+            self._create_alien(alien_number)
+    
+    def _create_alien(self, alien_number):
+            alien = Alien(self)
+            alien_width = alien.rect.width
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)        
     
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
