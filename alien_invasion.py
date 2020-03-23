60d7b20965f1cc1e86d90423155bb05400b2397a
TonyBHerrera <tonybherrera@yahoo.com>
2020-03-22 18:42:10 -0600

    Adding in first space ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,7 +5,7 @@ import pygame
 from settings import Settings 
 from ship import Ship
 from bullet import Bullet
-
+from alien import Alien
 class AlienInvasion:
     """ Overall clss to manage game assets and behavior"""
 
@@ -21,6 +21,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
         
 
     def run_game(self):
@@ -72,12 +75,19 @@ class AlienInvasion:
             if bullet.rect.bottom<= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
+    
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
-    
+        self.aliens.draw(self.screen)
+
         pygame.display.flip()
 if __name__ == '__main__':
     # Make a game instance and run the game 
