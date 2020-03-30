62143b3e1d83ee6cef0cac3d6f10222a81c95926
TonyBHerrera <tonybherrera@yahoo.com>
2020-03-29 21:34:23 -0600

    making fleet move from left to right

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             
     def _check_events(self):        
@@ -75,6 +76,13 @@ class AlienInvasion:
             if bullet.rect.bottom<= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """Update the postions of all aliens in the fleet"""
+        """check if the fleet is at an edge,
+        then update the postions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Make an alien.
@@ -103,6 +111,19 @@ class AlienInvasion:
             alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
             self.aliens.add(alien)        
     
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break 
+
+    def _change_fleet_direction(self):
+        """drop the entire fleet and change the fleets direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1     
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
